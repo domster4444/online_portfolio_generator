cf7a8559ad2645055e9d6e9578bd0b6a6d16d280
Kshitiz <kshitiz.stha11@gmail.com>
2021-12-01 04:01:22 -0500

    feat(form): use formik for handling form data

--- client/src/modules/Register/Register.tsx
+++ client/src/modules/Register/Register.tsx
@@ -1,7 +1,53 @@
 import React from 'react';
+import { useFormik } from 'formik';
 
 const Register = () => {
-  return <div>this is register page</div>;
+  const formik = useFormik({
+    initialValues: {
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+  return (
+    <section id="register-form">
+      <form onSubmit={formik.handleSubmit}>
+        <input
+          type="text"
+          id="firstName"
+          name="firstName"
+          onChange={formik.handleChange}
+          value={formik.values.firstName}
+        />
+        <input
+          type="text"
+          id="lastName"
+          name="lastName"
+          onChange={formik.handleChange}
+          value={formik.values.lastName}
+        />
+        <input
+          type="email"
+          id="email"
+          name="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+        />
+        <input
+          type="password"
+          id="password"
+          name="password"
+          onChange={formik.handleChange}
+          value={formik.values.password}
+        />
+        <button type="submit">Register</button>
+      </form>
+    </section>
+  );
 };
 
 export default Register;
