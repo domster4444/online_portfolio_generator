db53027313176ad89a72bbf229bd7049a0188b63
Kshitiz <kshitiz.stha11@gmail.com>
2021-12-01 02:53:40 -0500

    fix(dependency): minor issues

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,16 @@
+/* eslint-disable import/no-unresolved */
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import Navigation from 'library/common/components/Toolbar/Navigation';
 import LanguageSwitcher from './library/common/components/LanguageSwitcher';
+
 // eslint-disable-next-line func-names
 const App = function () {
   //* react international lang  middleware
   const { t } = useTranslation();
-
   return (
     <>
+      <Navigation />
       <LanguageSwitcher />
       {t('loginRegisterPage.descritpionParagraph.firstLine')}
     </>
--- client/src/library/common/components/Toolbar/Toolbar.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Toolbar = function () {
-  return <div>hello</div>;
-};
-
-export default Toolbar;
