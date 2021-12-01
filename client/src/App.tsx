f157c20ecc7ce8de44931fe4f55724fbfd02f5d7
Kshitiz <kshitiz.stha11@gmail.com>
2021-12-01 02:59:07 -0500

    fix(eslint_error): cannot resolve module, configure eslint rules

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable import/no-unresolved */
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import Navigation from 'library/common/components/Toolbar/Navigation';
