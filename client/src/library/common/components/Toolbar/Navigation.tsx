f877939b75c58bb4be389e1005c566a342ea248c
domster4444 <kshitiz.stha11@gmail.com>
2022-01-07 10:13:41 -0500

    fix(navigation): not to show home-nav if user loggedin

--- client/src/library/common/components/Toolbar/Navigation.tsx
+++ client/src/library/common/components/Toolbar/Navigation.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { RootStateOrAny, useDispatch, useSelector } from 'react-redux';
-import { logout } from 'library/common/components/stateSlices/loginSlice';
+import { RootStateOrAny, useSelector } from 'react-redux';
+
+import { Link } from 'react-router-dom';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -12,51 +12,30 @@ import Drawer from 'library/common/components/Drawer/Drawer';
 
 // eslint-disable-next-line react/function-component-definition
 export default function Navigation() {
-  const navigate = useNavigate();
   const { loggedInUser } = useSelector((state: RootStateOrAny) => state.login);
-  const dispatch = useDispatch();
 
-  const logoutSubmitHandler = () => {
-    // @ts-ignore: Unreachable code error
-    dispatch(logout());
-    localStorage.removeItem('loggedInUser');
-    navigate('/');
-  };
+  if (loggedInUser === null) {
+    return (
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Drawer />
 
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          {loggedInUser ? (
-            <div>
-              {loggedInUser.firstName}
-              <button
-                className="dropdown-item"
-                type="button"
-                onClick={logoutSubmitHandler}
-              >
-                Logout
-              </button>
-            </div>
-          ) : (
-            <>
-              <Drawer />
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              <Link to="/">Home Page</Link>
+            </Typography>
 
-              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                <Link to="/">Home Page</Link>
-              </Typography>
+            <Button color="inherit">
+              <Link to="/login">Login</Link>
+            </Button>
 
-              <Button color="inherit">
-                <Link to="/login">Login</Link>
-              </Button>
-
-              <Button color="inherit">
-                <Link to="/register">Register</Link>
-              </Button>
-            </>
-          )}
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
+            <Button color="inherit">
+              <Link to="/register">Register</Link>
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    );
+  }
+  return <div style={{ display: 'none' }} />;
 }
